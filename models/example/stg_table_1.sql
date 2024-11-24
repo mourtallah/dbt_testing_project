d2d88c8565ab95f4b61918693f618c2f430b1ee4
Mourtallah Faye <serigne.faye@icloud.com>
2024-11-24 00:08:34 +0000

    debugging stg models

--- models/example/stg_table_1.sql
+++ models/example/stg_table_1.sql
@@ -2,5 +2,5 @@
 SELECT *
 FROM {{ source("test_table_sources", "bikeshare_trips") }}
 {% if is_incremental() %}
-WHERE {{ source("test_table_sources", "bikeshare_trips" ) }}.timestamp > (SELECT MAX(timestamp) from {{ this }})
+WHERE {{ source("test_table_sources", "bikeshare_trips" ) }}.start_time > (SELECT MAX(start_time) from {{ this }})
 {% endif %}
--- models/example/stg_table_2.sql
+++ models/example/stg_table_2.sql
@@ -2,5 +2,5 @@
 SELECT *
 FROM {{ source("test_table_sources", "bikeshare_stations" ) }}
 {% if is_incremental() %}
-WHERE {{ source("test_table_sources", "bikeshare_stations" ) }}.timestamp > (SELECT MAX(timestamp) FROM {{ this }})
+WHERE {{ source("test_table_sources", "bikeshare_stations" ) }}.start_time > (SELECT MAX(start_time) FROM {{ this }})
 {% endif %}
