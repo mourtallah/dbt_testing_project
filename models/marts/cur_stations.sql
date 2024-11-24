a899e7ed0a0839585cae1931933e2f4c54550d22
Mourtallah Faye <serigne.faye@icloud.com>
2024-11-24 00:44:37 +0000

    retouch incremental logic in curated; revise snapshots

--- models/marts/cur_stations.sql
+++ models/marts/cur_stations.sql
@@ -2,5 +2,5 @@
 SELECT *
 FROM {{ ref("stg_stations") }}
 {% if is_incremental() %}
-WHERE {{ ref("stg_stations") }}.start_time > (SELECT MAX(start_time) FROM {{ this }})
+WHERE modified_date > (SELECT MAX(modified_date) FROM {{ this }})
 {% endif %}
--- models/marts/cur_trips.sql
+++ models/marts/cur_trips.sql
@@ -2,5 +2,5 @@
 SELECT *
 FROM {{ ref("stg_trips") }}
 {% if is_incremental() %}
-WHERE {{ ref("stg_trips") }}.start_time > (SELECT MAX(start_time) from {{ this }})
+WHERE start_time > (SELECT MAX(start_time) from {{ this }})
 {% endif %}
